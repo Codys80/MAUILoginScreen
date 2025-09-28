8b69b9ea03b6dca50c9ca24e43e86bef5e1a6de0
Codys80 <131345060+Codys80@users.noreply.github.com>
2025-09-28 23:12:41 +0200

    Dodanie projektu

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace LoginWindow
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿namespace LoginWindow
+{
+    public partial class MainPage : ContentPage
+    {
+        string emailUser = "";
+        string passwordUser = "";
+        string email = "admin";
+        string password = "admin";
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnLoginClicked(object sender, EventArgs e)
+        {
+            emailUser = emailEntry.Text;
+            passwordUser = passwordEntry.Text;
+            Notifications.Text = "Witaj";
+            if (emailUser != "admin") { Notifications.Text = "Błędny email"; }
+            if (passwordUser != "admin") { Notifications.Text = "Błędne hasło"; }
+            
+
+            //SemanticScreenReader.Announce(Notifications.Text);
+        }
+    }
+
+}
